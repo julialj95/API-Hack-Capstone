1815c5372616a9d08bf18948a971e19a001f6423
Julia Johnson <julialj95@gmail.com>
2020-12-04 11:27:45 -0600

    wireframes with minimal js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function getRecs(searchTerm){
+function getRecs(recSearchTerm){
 
   const searchUrl = 'https://tastedive.com/api/similar'
   const apiKey = '393554-StudentC-MF9HETN1'
 
-  const params = {
+  const recParams = {
     q: searchTerm + '+inTitle:',
     k: apiKey,
     type: 'book',
-    callback
+    callback: unknown
   }
 
-  const queryString = formatRecQueryParams(params)
+  const queryString = formatRecQueryParams(recParams)
   const url = searchUrl + '?' + queryString
   console.log(url)
 
@@ -21,15 +21,15 @@ function getRecs(searchTerm){
 }
 
 function formatRecQueryParams(params){
-  const queryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
-  return queryItems.join('&')
+  const recQueryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return recQueryItems.join('&')
 }
 
 function watchRecForm(){
   $('#js-book-search-form').on('submit', event => {
     event.preventDefault()
-    const searchTerm = $("#js-search-term").val()
-    getRecs(searchTerm)
+    const recSearchTerm = $("#js-search-term").val()
+    getRecs(recSearchTerm)
   })
 }
 
@@ -39,26 +39,58 @@ function watchRecForm(){
 
 function getBooksToBuy(){
   const searchUrl = 'https://www.googleapis.com/books/v1/volumes'
+  const apiKey = 'AIzaSyDEde2t4gSXAMcNjSVn56s2RfIu6G7R5ZM'
+
+  const googleParams = {
+    q: shopSearchTerm + '+intitle:',
+    printType: books,
+    key: apiKey
+  }
+
 
 }
 
-function formatShopQueryParams(params){
-
+function formatShopQueryParams(googleParams){
+  const shopQueryItems = Object.keys(googleParams).map(key => `${encodeURIComponent(key)} = ${encodeURIComponent(googleParams[key])}`)
+  return shopQueryItems.join('&')
 }
 
 function watchShopForm(){
+  $('#js-search-term').submit (event => {
+    event.preventDefault();
+    const shopSearchTerm = $("#js-search-term").val();
+    getBooksToBuy(shopSearchTerm)
+  })
 
 }
 
-// function getCarbonOffsetData(){
+function displayShopResults(responseJson){
+$('.results').empty()
+$('.results').append(`<div class="item">
+  <h2>${responseJson[i].volumeInfo.title}</h2>
+  <h3>${responseJson[i].volumeInfo.author}</h3>
+  <img src="${responseJson[i].imageLinks.thumbnail}" alt="${responseJson[i].volumeInfo.title} cover photo">
+  <p>${responseJson[i].volumeInfo.description}</p>
+</div>`)
+}
+function getCarbonOffsetData(){
+  const apiKey = 'aQLGLDqsUuR5GPHa3OSVZQ'
+  const searchUrl = 'https://www.carboninterface.com/api/v1/estimates'
 
-// }
+  const options = {
+    header: new Headers({
+      Authorization: 'Bearer ' + apiKey
+    })
+  }
+}
 
 // function watchCarbonForm(){
 
 // }
 
+function formatCarbonQueryParams(carbonQueryParams){
 
+}
 
 
 

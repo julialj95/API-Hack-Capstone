c9acc7183dba6bd523edde009f0d29de6a4a6df8
Julia Johnson <julialj95@gmail.com>
2020-12-08 18:55:18 -0600

    added recommendation results return upon search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getRecTitles(recSearchTerm){
+function getRecTitlesUrl(recSearchTerm){
 
   const searchUrl = 'https://tastedive.com/api/similar'
   const apiKey = '393554-StudentC-MF9HETN1'
@@ -7,7 +7,7 @@ function getRecTitles(recSearchTerm){
     q: "book:" + recSearchTerm,
     k: apiKey,
     type: 'books',
-    callback: 'callbackData'
+    callback: 'getRecInfo'
   }
 
   const queryString = formatRecQueryParams(recParams)
@@ -16,51 +16,15 @@ function getRecTitles(recSearchTerm){
   return recUrl
 }
 
-function getRecInfo(recTitleJson){
-  const recInfoBaseUrl = 'https://www.googleapis.com/books/v1/volumes'
-  const googleApiKey = 'AIzaSyDEde2t4gSXAMcNjSVn56s2RfIu6G7R5ZM'
-
-  // for (let i = 0; i < recTitleJson.Results.length; i++){
-  
-    const recInfoParams = {
-    q: recTitleJson.Similar.Results[0].Name + '+intitle:',
-    printType: 'books',
-    key: googleApiKey
-    }
-  const recInfoQueryString = formatRecInfoQueryParams(recInfoParams)
-  const recInfoUrl = recInfoBaseUrl + '?' + recInfoQueryString
-  console.log(recInfoUrl)
-
-  const recInfoArray = []
-  
-  fetch(recInfoUrl)
-  .then(recInfoResponse => recInfoResponse.json())
-  .then(recInfoResponseJson => recInfoArray.push(recInfoResponseJson))
-  .catch(err => console.log(err))
-  console.log(recInfoArray)
-  return recInfoArray
-  }
-
-
-function callbackData(recTitleJson){
-  console.log(recTitleJson)
-  // displayRecResults(recTitleJson)
-  return recTitleJson
-
-}
-
 function formatRecQueryParams(recParams){
   const recQueryItems = Object.keys(recParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(recParams[key])}`);
   return recQueryItems.join('&')
 }
-function formatRecInfoQueryParams(recInfoParams){
-  const recInfoQueryItems = Object.keys(recInfoParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(recInfoParams[key])}`);
-  return recInfoQueryItems.join('&')
-}
+
 
 function insertScript(recSearchTerm){
     const newScriptElement = document.createElement('script');
-    newScriptElement.setAttribute('src', getRecTitles(recSearchTerm));
+    newScriptElement.setAttribute('src', getRecTitlesUrl(recSearchTerm));
     newScriptElement.setAttribute('id', 'jsonp');
     const oldScriptElement = document.getElementById('jsonp');
     
@@ -74,83 +38,107 @@ function insertScript(recSearchTerm){
     }
 }
 
-function watchRecForm(recTitleJson){
+
+function getRecInfo(recTitleJson){
+  console.log(recTitleJson)
+
+  const recInfoBaseUrl = 'https://www.googleapis.com/books/v1/volumes'
+  const googleApiKey = 'AIzaSyDEde2t4gSXAMcNjSVn56s2RfIu6G7R5ZM'
+  const recInfoArray = [];
+  for (let i = 0; i < recTitleJson.Similar.Results.length; i++){
+  
+    const recInfoParams = {
+    q: recTitleJson.Similar.Results[i].Name,
+    printType: 'books',
+    key: googleApiKey
+    }
+  const recInfoQueryString = formatRecInfoQueryParams(recInfoParams)
+  const recInfoUrl = recInfoBaseUrl + '?' + recInfoQueryString
+    recInfoArray.push(fetch(recInfoUrl))
+  }
+Promise.all(recInfoArray)
+  .then(results => {
+  return Promise.all(results.map(r => r.json()))
+  })
+  .then(jsonRecs => {
+  console.log(jsonRecs)
+  displayRecResults(jsonRecs)
+  })
+  .catch(err => console.log(err))
+}
+
+
+function formatRecInfoQueryParams(recInfoParams){
+  const recInfoQueryItems = Object.keys(recInfoParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(recInfoParams[key])}`);
+  return recInfoQueryItems.join('&')
+}
+
+function watchRecForm(){
   $('#js-book-search-form').on('submit', event => {
     event.preventDefault()
     const recSearchTerm = $("#js-search-term").val()
-    console.log(recSearchTerm)
     insertScript(recSearchTerm)
-    getRecTitles(recSearchTerm)
-    getRecInfo(recTitleJson)
+    getRecTitlesUrl(recSearchTerm)
   })
 }
 
-function displayRecResults(recTitleJson){
+function displayRecResults(resultsJson){
   $("#js-rec-results").empty()
-  for (let i = 0; i < 20; i++){
-    $("#js-rec-results").append(`
-    <div class="item">
-    <h2>${recTitleJson.Similar.Results[i].Name}</h2>`)
+  for (let i = 0; i < 12; i++){
+  $("#js-rec-results").append(`
+  <div class="item">
+    <h2>${resultsJson[i].items[0].volumeInfo.title}</h2>
+    <h3>${resultsJson[i].items[0].volumeInfo.authors}</h3>
+    <img src="${resultsJson[i].items[0].volumeInfo.imageLinks.thumbnail}" alt="${resultsJson[i].items[0].volumeInfo.title} cover photo">
+    <p>${resultsJson[i].items[0].volumeInfo.description}</p>
+    <a href="shop.html">Shop this book</a>
+  </div>`)}
+  $('.results').removeClass('hidden')
   }
-  $(".results").removeClass('hidden')
-}
-// function displayRecResults(recTitleJson, recInfoResponseJson){
-//   $("#js-rec-results").empty()
-//   for (let i = 0; i < 20; i++){
-//     console.log(recTitleJson.Similar.Results[i].Name)
-//   $("#js-rec-results").append(`
-//   <div class="item">
-//     <h2>${recTitleJson.Similar.Results[i].Name}</h2>
-//     <h3>${recInfoResponseJson[i].volumeInfo.author}</h3>
-//     <img src="${recInfoResponseJson[i].imageLinks.thumbnail}" alt="${recInfoResponseJson[i].volumeInfo.title} cover photo">
-//     <p>${recInfoResponseJson[i].volumeInfo.description}</p>
-//     <a href="shop.html">Shop this book</a>
-//   </div>`)}
-//   $('.results').removeClass('hidden')
+
+
+
+// function watchShopLinkOnRecPage(){
+//   $("#").on('click', event=> {
+//     event.preventDefault;
+//     getBooksToBuy(recResponseJson)
+//   })
+// }
+
+// function getBooksToBuy(shopSearchTerm){
+//   const shopSearchUrl = 'https://www.googleapis.com/books/v1/volumes'
+//   const googleApiKey = 'AIzaSyDEde2t4gSXAMcNjSVn56s2RfIu6G7R5ZM'
+
+//   const googleParams = {
+//     q: shopSearchTerm + '+intitle:',
+//     printType: 'books',
+//     key: googleApiKey
 //   }
 
+//   const shopQueryString = formatShopQueryParams(googleParams)
+//   const url = shopSearchUrl + '?' + shopQueryString
+//   console.log(url)
 
-function watchShopLinkOnRecPage(){
-  $("#").on('click', event=> {
-    event.preventDefault;
-    getBooksToBuy(recResponseJson)
-  })
-}
+//   fetch(url)
+//   .then(shopResponse => shopResponse.json())
+//   .then(shopResonseJson => console.log(shopResonseJson))
+//   .catch(err => console.log(err))
 
-function getBooksToBuy(shopSearchTerm){
-  const shopSearchUrl = 'https://www.googleapis.com/books/v1/volumes'
-  const googleApiKey = 'AIzaSyDEde2t4gSXAMcNjSVn56s2RfIu6G7R5ZM'
+// }
 
-  const googleParams = {
-    q: shopSearchTerm + '+intitle:',
-    printType: 'books',
-    key: googleApiKey
-  }
+// function formatShopQueryParams(googleParams){
+//   const shopQueryItems = Object.keys(googleParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(googleParams[key])}`)
+//   return shopQueryItems.join('&')
+// }
 
-  const shopQueryString = formatShopQueryParams(googleParams)
-  const url = shopSearchUrl + '?' + shopQueryString
-  console.log(url)
+// function watchShopForm(){
+//   $('#book-shop-form').submit(event => {
+//     event.preventDefault();
+//     const shopSearchTerm = $("#js-book-shop").val()
+//     getBooksToBuy(shopSearchTerm)
+//   })
 
-  fetch(url)
-  .then(shopResponse => shopResponse.json())
-  .then(shopResonseJson => console.log(shopResonseJson))
-  .catch(err => console.log(err))
-
-}
-
-function formatShopQueryParams(googleParams){
-  const shopQueryItems = Object.keys(googleParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(googleParams[key])}`)
-  return shopQueryItems.join('&')
-}
-
-function watchShopForm(){
-  $('#book-shop-form').submit(event => {
-    event.preventDefault();
-    const shopSearchTerm = $("#js-book-shop").val()
-    getBooksToBuy(shopSearchTerm)
-  })
-
-}
+// }
 
 // function displayShopResults(recResponseJson){
 //   $('.results').empty()
@@ -186,4 +174,4 @@ function watchShopForm(){
 
 
   $(watchRecForm)
-  $(watchShopForm)
+  // $(watchShopForm)

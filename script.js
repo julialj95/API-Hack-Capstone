0d586e29cbdac6787690ffea6bc4286ff2464804
Julia Johnson <julialj95@gmail.com>
2020-12-12 19:03:49 -0600

    responsive styles added

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function insertScript(recSearchTerm){
 
 function getRecInfo(recTitleJson){
   if (recTitleJson.Similar.Results.length === 0){
-    $(".loading").addClass('hidden')
-    $(".results").html("<p>Sorry! No results were found for that search. Try a different book title.</p>")
+    $(".loadingBox").addClass('hidden')
+    $(".results").html("<p>Sorry! No results were found for that search. Try a different book title.</p>").addClass('bg-box')
   }
   else {
   const recInfoBaseUrl = 'https://www.googleapis.com/books/v1/volumes'
@@ -84,20 +84,17 @@ function formatRecInfoQueryParams(recInfoParams){
 function watchRecForm(){
   $('#js-book-search-form').on('submit', event => {
     event.preventDefault()
-    $(".loading").removeClass("hidden")
+    $(".loadingBox").removeClass("hidden")
     const recSearchTerm = $("#js-search-term").val()
     insertScript(recSearchTerm)
     getRecTitlesUrl(recSearchTerm)
     $('#js-search-term').val('')
     $(".shop").addClass('hidden')
-    $("#js-shop-results").html(`<div class="container">
-    <h1>Buying Options:</h1>
-    </div>`)
   })
 }
 
 function displayRecResults(resultsJson){
-  $(".loading").addClass('hidden')
+  $(".loadingBox").addClass('hidden')
   $("#js-rec-results").empty()
   for (let i = 0; i < 12; i++){
   $("#js-rec-results").append(`
@@ -108,29 +105,25 @@ function displayRecResults(resultsJson){
     <p>${resultsJson[i].items[0].volumeInfo.description}</p>
     <button class="js-shop">Shop this book</button>
     <div class="hidden-id">${i}</div>
-    <div class="hidden"><p>Scroll down to see results</p></div>
-    
+    <div class="hidden shopResults"></div>
   </div>`)}
   $('.results').removeClass('hidden')
   }
 
-{/* <div class="hidden shop-results" id="js-shop-results"></div> */}
-
 function watchShopLinkOnRecPage(){
   $('body').on('click','button.js-shop', event=> {
     event.preventDefault
-    $("#js-shop-results").html(`<div class="container">
-    <h1>Buying Options:</h1>
-  </div>`)
+    $(".shopResults").empty()
     $(event.currentTarget).next().next().removeClass('hidden')
     const shopSearchNumber = $(event.currentTarget).next().text()
+    const shopDisplay = $(event.currentTarget).next().next()
     console.log(shopSearchNumber)
-    displayShopResults(bookResults, shopSearchNumber)
+    displayShopResults(bookResults, shopSearchNumber, shopDisplay)
     
   })
 }
 
-function displayShopResults(bookResults, shopSearchNumber){
+function displayShopResults(bookResults, shopSearchNumber, shopDisplay){
   console.log(bookResults[shopSearchNumber])
   let shopCounter = 0
   for (let i = 0; i < bookResults[shopSearchNumber].items.length; i++) {
@@ -140,22 +133,22 @@ function displayShopResults(bookResults, shopSearchNumber){
     if(bookResults[shopSearchNumber].items[i].saleInfo.saleability === "FOR_SALE" && currentTitle.includes(titleToMatch)) {
     shopCounter = shopCounter + 1
     console.log('appending')
-    $("#js-shop-results").append(`
-    <div class="shop-item bg-box">
-      <h2>Title: ${currentTitle}</h2>
-      <h3>Price: ${bookResults[shopSearchNumber].items[i].saleInfo.listPrice.amount} ${bookResults[shopSearchNumber].items[i].saleInfo.listPrice.currencyCode}</h3>
-      <a href="${bookResults[shopSearchNumber].items[i].saleInfo.buyLink}" target="_blank">Buy this book on Google</a>
+    $(shopDisplay).append(`
+    <div class="shop-item shop-box">
+      <h2 class="dark-font">Title: ${currentTitle}</h2>
+      <h3 class="dark-font">Price: ${bookResults[shopSearchNumber].items[i].saleInfo.listPrice.amount} ${bookResults[shopSearchNumber].items[i].saleInfo.listPrice.currencyCode}</h3>
+      <a href="${bookResults[shopSearchNumber].items[i].saleInfo.buyLink}" target="_blank" class="light-font">Buy this book on Google</a>
     </div>
     `)
     }
   }
-  $("#js-shop-results").removeClass("hidden")
-  errorMessage(shopCounter)
+  $(".shopResults").removeClass("hidden")
+  errorMessage(shopCounter, shopDisplay)
 }
 
-function errorMessage(shopCounter){
+function errorMessage(shopCounter, shopDisplay){
   if (shopCounter === 0){
-    $("#js-shop-results").append(`
+    $(shopDisplay).append(`
     <div class="shop-item">
       <p>Sorry! That book is not currently for sale.</p>
     </div>`)
